443576e5eabc2231a538acde61265a2a3aaf5793
disturguy <billmarko1991@gmail.com>
2021-09-11 20:01:04 +0300

    a complete in memory microserver without auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Query } from '@nestjs/common/decorators/http/route-params.decorator';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -10,18 +11,28 @@ export class UsersController {
     constructor (private usersService: UsersService) {}
 
     @ApiOkResponse({type: User, isArray: true})
+    @ApiQuery({name:"name", required: false})
     @Get()
-    getUsers(): User[] {
-        return this.usersService.findAll();
+    getUsers(@Query('name') name: string): User[] {
+        return this.usersService.findAll(name);
     }
 
     @ApiOkResponse({type: User, description:"The user"})
+    @ApiNotFoundResponse()
     @Get(':id')
-    getUSerById(@Param('id') id: string ): User {
-        return this.usersService.findById(Number(id));
+    getUSerById(@Param('id', ParseIntPipe) id: number ): User {
+
+        const user =  this.usersService.findById(id);
+
+        if (!user) {
+            throw new NotFoundException;
+        }
+
+        return user;
     }
 
     @ApiCreatedResponse({type: User})
+    @ApiBadRequestResponse()
     @Post()
     createUser(@Body() body: CreateUserDto): User {
         return this.usersService.createUsers(body);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,9 +4,16 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-    private users: User[] = [{id: 0, name: 'Bill'}];
+    private users: User[] = [
+        { id: 0, name: 'Bill' },
+        { id: 1, name: 'Antonis' },
+        { id: 2, name: 'Charis' }
+    ];
 
-    findAll(): User[]{
+    findAll(name?: string): User[]{
+        if (name){
+           return this.users.filter((user) => user.name === name);
+        }
         return this.users;
     }
 
